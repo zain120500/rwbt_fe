20f3c12ac496bed4670539538b0a7b19177ed710
Dendy Zain <dendyzain@172-10-66-74.lightspeed.jcsnms.sbcglobal.net>
2025-08-14 13:44:50 +0700

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,36 @@
+"use client";
+
 import Image from "next/image";
 
+declare global {
+  interface Window {
+    AndroidPrint?: {
+      printText: (text: string) => void;
+    };
+  }
+}
+
+  const handlePrint = () => {
+    const textToPrint = "Hello dari WebView!\n\n";
+    // Cek apakah AndroidPrint tersedia
+    if (window?.AndroidPrint) {
+      // @ts-ignore → abaikan warning TS
+      window.AndroidPrint.printText(textToPrint);
+    } else {
+      alert("AndroidPrint tidak tersedia");
+    }
+  };
+  
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+       <button
+          onClick={handlePrint}
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          Print Thermal
+        </button>
+      
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
